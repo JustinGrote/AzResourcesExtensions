0eb16156ebe38ef6f608d3847f10ddfb2f14eff1
Justin Grote <justingrote@github-viewmysite.com>
2021-01-21 14:42:36 -0800

    👷 Add Module Manifest

--- AzResourceExtensions/AzResourcesExtensions.psm1
+++ AzResourceExtensions/AzResourcesExtensions.psm1
@@ -1,7 +1,7 @@
 $publicFunctions = @()
-foreach ($ScriptPathItem in 'Private','Public','Classes','Helpers') {
+$PublicFunctions = foreach ($ScriptPathItem in 'Private','Public','Classes','Helpers') {
     $ScriptSearchFilter = [io.path]::Combine($PSScriptRoot, $ScriptPathItem, '*.ps1')
-    $PublicFunctions = Get-ChildItem -Recurse -Path $ScriptSearchFilter -Exclude '*.Tests.ps1' -ErrorAction SilentlyContinue | 
+    Get-ChildItem -Recurse -Path $ScriptSearchFilter -Exclude '*.Tests.ps1' -ErrorAction SilentlyContinue | 
         Foreach-Object {
             . $PSItem
             if ($ScriptPathItem -eq 'Public') {
